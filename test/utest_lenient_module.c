b30a3469a5f87c6dd57b25d01a26ef477d488f8d
Aleksi Backman <bddubbb@protonmail.com>
2025-03-17 14:57:53 +0200

    Add module wrapper constructor test

--- test/utest_lenient_module.c
+++ test/utest_lenient_module.c
@@ -10,7 +10,23 @@ void teardown(void) {
 }
 #endif
 
-Test(lenient_module, TODO_write_a_test_name)
+Test(lenient_module, module_wrapper_constructor_returns_an_instance)
 {
-    cr_expect(1 == 0);
+    struct module_wrapper* module_wrapper;
+
+    module_wrapper = NULL;
+
+    module_wrapper = module_wrapper_construct_to_heap(1, 2);
+
+    cr_assert(ne(module_wrapper, NULL));
+
+    module_wrapper_destroy(module_wrapper);
+    module_wrapper = NULL;
 }
+
+// Test(sample, passing, .signal = SIGSEGV) {
+#if 0
+Test(lenient_module, module_wrapper_destructor)
+{
+}
+#endif
